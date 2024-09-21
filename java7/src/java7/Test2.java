7fc6197082e7bb2a1cb6367f6f1a60c8448e7f4f
Ramnath <Ramnath@DESKTOP-R8P0C6R>
2024-09-21 11:15:46 -0400

    add m4

--- java7/src/java7/Test2.java
+++ java7/src/java7/Test2.java
@@ -12,5 +12,17 @@ public class Test2 {
 	{
 		System.out.println("m2");
 	}
+	
+	public void m3()
+	{
+		System.out.println("m3");
+	}
+	
+	public void m4()
+	{
+		System.out.println("m4");
+	}
+
+
 
 }
